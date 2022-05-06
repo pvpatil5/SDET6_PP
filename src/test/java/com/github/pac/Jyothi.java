fc6401e9af0216b8be5beb06dcbac3db86c9467d
Welcome <Welcome@DESKTOP-JDBRO8S>
2022-05-06 11:20:57 +0530

    JYothi

---
+++ src/test/java/com/github/pac/Jyothi.java
@@ -0,0 +1,12 @@
+package com.github.pac;
+
+import org.testng.annotations.Test;
+
+public class Jyothi {
+
+	@Test
+	public void p1()
+	{
+		System.out.println("hi pavan sir this is jyothi basai");
+	}
+}
